5c7b872559c260b5e9da7b4bda49cfece65cdc32
bharathwajan <rbw1906@gmail.com>
2022-07-30 17:24:28 +0530

    added changes in items.py

--- amazon_scrapper/items.py
+++ amazon_scrapper/items.py
@@ -2,10 +2,8 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
-
+#hello please add me to git
 import scrapy
-
-
 class AmazonScrapperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
